2a58fd1cd6febfd1e5bd43553003e52c6e5bc17d
Iván Mervich <imervich@gmail.com>
2014-08-12 14:07:31 -0500

    load characters from db or plist

---
+++ LostCharacterDatabase/LostCharacter.h
@@ -0,0 +1,18 @@
+//
+//  LostCharacter.h
+//  LostCharacterDatabase
+//
+//  Created by Iván Mervich on 8/12/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface LostCharacter : NSManagedObject
+
+@property (nonatomic, retain) NSString * actor;
+@property (nonatomic, retain) NSString * passenger;
+
+@end
---
+++ LostCharacterDatabase/LostCharacter.m
@@ -0,0 +1,17 @@
+//
+//  LostCharacter.m
+//  LostCharacterDatabase
+//
+//  Created by Iván Mervich on 8/12/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "LostCharacter.h"
+
+
+@implementation LostCharacter
+
+@dynamic actor;
+@dynamic passenger;
+
+@end
